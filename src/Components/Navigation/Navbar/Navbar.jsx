6489345dbdbf0f69cddd6e0f1a210f9803fe53c9
manojbelbase <manojbelbase56@gmail.com>
2023-06-04 22:32:50 +0545

    hello

--- src/Components/Navigation/Navbar/Navbar.jsx
+++ src/Components/Navigation/Navbar/Navbar.jsx
@@ -1,7 +1,26 @@
 import React from "react";
-
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
+import { GoSearch } from "react-icons/go";
+// import { useNavigate } from "react-router-dom";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="fixed top-0 bg-secondColor w-screen shadow-md shadow-slate-800">
+      <div
+        className="w-full h-full flex justify-between items-center text-white
+      pl-4"
+      >
+        <div className="logo bg-red-400 rounded-full h-10 w-10"></div>
+        <div className="searchbox flex items-center relative">
+          <GoSearch className="absolute left-3 text-black" />
+          <input
+            type="search"
+            className="text-black focus:outline-none rounded-3xl p-2 pl-10 font-4xl"
+            placeholder="search..."
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/Hoc/Layouts/Layout.jsx
+++ src/Hoc/Layouts/Layout.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Sidebar from "../../Components/Navigation/Sidebar/Sidebar";
+import Navbar from "../../Components/Navigation/Navbar/Navbar";
 
 const Layout = () => {
   return (
     <div>
       <div className="grid grid-cols-12">
         <div className="col-span-2 bg-red-500 h-screen sticky top-0 left-0">
-          hii
+          <Sidebar />
         </div>
-        <div className="col-span-10 bg-yellow-500">hello</div>
+        <div className="col-span-10 bg-yellow-500 relative">
+          <Navbar />
+        </div>
       </div>
 
-      <div className="absolute top-20 left-64">
+      <div className="absolute top-4 left-48">
         <Outlet />
       </div>
     </div>
