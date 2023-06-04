17f6bf5defc96968a8fb2abd480883188a6ca339
jivan aryal <jeevanaryal7947@gmail.com>
2023-06-04 22:19:33 +0545

    css

--- src/Hoc/Layouts/Layout.jsx
+++ src/Hoc/Layouts/Layout.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "../../Components/Navigation/Navbar/Navbar";
+import Sidebar from "../../Components/Navigation/Sidebar/Sidebar";
 
 const Layout = () => {
   return (
     <div>
       <div className="grid grid-cols-12">
         <div className="col-span-2 bg-red-500 h-screen sticky top-0 left-0">
-          hii
+          <Sidebar />
         </div>
-        <div className="col-span-10 bg-yellow-500">hello</div>
-      </div>
+        <div className="col-span-10 bg-yellow-500 relative">
+          <div>
+            <Navbar />
+          </div>
 
-      <div className="absolute top-20 left-64">
-        <Outlet />
+          <div className="w-11/12 mx-auto ">
+            <Outlet />
+          </div>
+        </div>
       </div>
     </div>
   );
