4ee65160cab113955dd421bd4b2bc2cd1c1b2add
jivan aryal <jeevanaryal7947@gmail.com>
2023-06-04 21:57:23 +0545

    gridf

--- src/Hoc/Layouts/Layout.jsx
+++ src/Hoc/Layouts/Layout.jsx
@@ -4,7 +4,14 @@ import { Outlet } from "react-router-dom";
 const Layout = () => {
   return (
     <div>
-      <div>
+      <div className="grid grid-cols-12">
+        <div className="col-span-2 bg-red-500 h-screen sticky top-0 left-0">
+          hii
+        </div>
+        <div className="col-span-10 bg-yellow-500">hello</div>
+      </div>
+
+      <div className="absolute top-20 left-64">
         <Outlet />
       </div>
     </div>
