433602597d61de89cd7545087859cc720febb84c
manojbelbase <manojbelbase56@gmail.com>
2023-06-07 22:51:11 +0545

    hi

--- src/Components/Pages/Dashboard/Dashboard.jsx
+++ src/Components/Pages/Dashboard/Dashboard.jsx
@@ -1,56 +1,54 @@
 import React from "react";
 
-
 const data = [
   {
-    title:"Available Position",
-    num:"24",
-    intro:"4 urgents needed"
+    title: "Available Position",
+    num: "24",
+    intro: "4 urgents needed",
   },
   {
-    title:"Available Position",
-    num:"24",
-    intro:"4 urgents needed"
+    title: "Available Position",
+    num: "24",
+    intro: "4 urgents needed",
   },
   {
-    title:"Available Position",
-    num:"24",
-    intro:"4 urgents needed"
+    title: "Available Position",
+    num: "24",
+    intro: "4 urgents needed",
   },
   {
-    title:"Available Position",
-    num:"24",
-    intro:"4 urgents needed"
+    title: "Available Position",
+    num: "24",
+    intro: "4 urgents needed",
   },
   {
-    title:"Available Position",
-    num:"24",
-    intro:"4 urgents needed"
+    title: "Available Position",
+    num: "24",
+    intro: "4 urgents needed",
   },
   {
-    title:"Available Position",
-    num:"24",
-    intro:"4 urgents needed"
+    title: "Available Position",
+    num: "24",
+    intro: "4 urgents needed",
   },
-]
+];
 
 const Dashboard = () => {
   return (
     <div>
       <div className="grid grid-cols-4 place-items-center">
-        {data.map((val,i)=>{
+        {data.map((val, i) => {
           return (
-            <div key={i} className="bg-red-500 p-4 rounded-xl"> 
+            <div key={i} className="bg-green-500 p-4 rounded-xl">
               <div>{val.title}</div>
               <div>{val.num}</div>
               <div>{val.intro}</div>
             </div>
-          )
+          );
         })}
       </div>
-      
     </div>
-  )
+  );
 };
 
 export default Dashboard;
