8f94943ec8431183e1be828d20f372aa13e0262d
jivan aryal <jeevanaryal7947@gmail.com>
2023-06-30 21:05:30 +0545

    hello

--- src/Components/Pages/Departments/AddDepartment.jsx
+++ src/Components/Pages/Departments/AddDepartment.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Formik, Form, Field } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import { toast, ToastContainer } from "react-toastify";
 import * as yup from "yup";
 import axios from "axios";
@@ -28,51 +28,50 @@ const AddDepartment = () => {
     });
   };
   return (
-    <div className="w-full px-10">
+    <div className="w-full px-6 sm:px-10">
       <Formik
         initialValues={{
           dept_name: "",
           dept_location: "",
         }}
         validationSchema={schema}
-        onSubmit={(val) => {
-          console.log(val);
-          postFormData(val);
+        onSubmit={(values) => {
+          postFormData(values);
+          toast.success("Form submitted successfully!");
         }}
       >
-        {({ handleSubmit }) => {
-          return (
-            <Form
-              onSubmit={handleSubmit}
-              encType="multipart/form-data"
-              className="mt-20"
-            >
-              <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
-                {FormField.map((val, i) => (
-                  <div key={i} className="mx-auto">
-                    <label htmlFor={val.name} className="text-sm font-semibold">
-                      {val.name}
-                    </label>
-                    <Field
-                      type={val.type}
-                      name={val.name}
-                      className="border border-gray-400 rounded-md py-2 px-2 w-full"
-                      placeholder={`Enter ${val.name}`}
-                    />
-                  </div>
-                ))}
-              </div>
-              <div className="flex justify-center mt-9">
-                <button
-                  type="submit"
-                  className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 transition-colors duration-300"
-                >
-                  Submit
-                </button>
-              </div>
-            </Form>
-          );
-        }}
+        {({ handleSubmit }) => (
+          <Form onSubmit={handleSubmit} className="mt-8">
+            <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
+              {FormField.map((field, index) => (
+                <div key={index}>
+                  <label htmlFor={field.name} className="text-sm font-semibold">
+                    {field.name}
+                  </label>
+                  <Field
+                    type={field.type}
+                    name={field.name}
+                    className="border border-gray-400 rounded-md py-2 px-3 w-full"
+                    placeholder={`Enter ${field.name}`}
+                  />
+                  <ErrorMessage
+                    name={field.name}
+                    component="div"
+                    className="text-red-500 text-sm mt-1"
+                  />
+                </div>
+              ))}
+            </div>
+            <div className="flex justify-center mt-9">
+              <button
+                type="submit"
+                className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 transition-colors duration-300"
+              >
+                Submit
+              </button>
+            </div>
+          </Form>
+        )}
       </Formik>
       <ToastContainer />
     </div>
--- src/Hoc/Layouts/Layout.jsx
+++ src/Hoc/Layouts/Layout.jsx
@@ -5,18 +5,16 @@ import Sidebar from "../../Components/Navigation/Sidebar/Sidebar";
 
 const Layout = () => {
   return (
-    <div className="flex">
-      {/* Sidebar */}
-      <div className="w-2/12 sm:w-1/5 px-6 fixed top-0 h-screen overflow-hidden max-h-screen shadow-xl">
-        <Sidebar />
+    <div className="grid grid-cols-12">
+      <div className="col-span-2">
+        <div className="sticky top-0 h-screen overflow-hidden max-h-screen shadow-xl">
+          <Sidebar />
+        </div>
       </div>
-
-      {/* Main Content */}
-      <div className="w-10/12 sm:w-4/5 ml-auto">
+      <div className="col-span-10 z-50">
         <div>
           <Navbar />
         </div>
-
         <div className="relative pl-2 -z-50 top-24 mx-auto">
           <Outlet />
         </div>
