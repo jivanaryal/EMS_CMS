0419dc9144a133693a0078c5348c4b763f8db48d
jivan aryal <jeevanaryal7947@gmail.com>
2023-07-04 20:12:53 +0545

    hello

--- src/Components/Pages/Dashboard/Dashboard.jsx
+++ src/Components/Pages/Dashboard/Dashboard.jsx
@@ -6,14 +6,14 @@ const data = [
   {
     title: "Total Department",
     num: "24",
-    intro: "Add Department",
+    intro: "View Department",
     colors: "#FFEFE7",
     path: "/department",
   },
   {
     title: "Total Employee",
     num: "24",
-    intro: "Add Employee",
+    intro: "View Employee",
     colors: "#E8F0FB",
     path: "/employee",
   },
@@ -45,40 +45,45 @@ const Dashboard = () => {
     });
   }, []);
 
+  // Sort the employees by salary in descending order
+  const sortedEmployees = [...employee].sort((a, b) => b.salary - a.salary);
+
+  // Get the top 5 employees with highest salary
+  const topEmployees = sortedEmployees.slice(0, 5);
+
   return (
     <div className="">
       <div className="flex gap-10">
-        <div className="grid grid-cols-1 gap-5   md:grid-cols-2 w-10/12">
+        <div className="grid grid-cols-1 gap-5 md:grid-cols-2 h-36 w-10/12">
           {data.map((val, i) => (
             <Link to={val.path} key={i}>
               <div
                 style={{ backgroundColor: val.colors }}
-                className="rounded-lg flex flex-col gap-2 items-center text-sm shadow-md shadow-blue-400 py-10 mx-2 sm:mx-4 md:mx-6 text-center hover:transition-all hover:scale-105 hover:delay-200 hover:duration-1000"
+                className="rounded-lg h-32 flex flex-col gap-2 items-center text-sm shadow-md shadow-blue-400 py-10 mx-2 sm:mx-4 md:mx-6 text-center hover:transition-all hover:scale-105 hover:delay-200 hover:duration-1000"
               >
-                <div className="text-lg">{val.title}</div>
+                <div className="text-lg font-bold">{val.title}</div>
                 <div>{val.num}</div>
-                <div>{val.intro}</div>
               </div>
             </Link>
           ))}
         </div>
-        <div className="employee_data px-6 shadow-xl bg-white rounded-lg w-full mx-4">
-          <h1 className="text-2xl font-bold mb-4">Top Employees</h1>
+        <div className="employee_data px-6 shadow-xl py-4 bg-white rounded-lg w-full mx-4">
+          <h1 className="text-2xl font-bold mb-4">Top Salary Employees</h1>
           <div>
-            {employee.slice(0, 6).map((emp, index) => (
+            {topEmployees.map((emp, index) => (
               <div key={index} className="flex items-center mb-4">
                 <img
                   src={`http://192.168.18.7:5000/${emp.image}`}
                   alt="Employee"
                   className="w-12 h-12 rounded-full mr-5"
                 />
-                <div className="grid grid-cols-5 gap-4 w-full  text-[#5E5E5E] font-bold items-center  ">
-                  <div className="text-gray-600  col-span-2">
+                <div className="grid grid-cols-5 gap-4 w-full text-sm text-[#5E5E5E] font-bold items-center">
+                  <div className="text-gray-700 capitalize col-span-2">
                     {emp.first_name} {emp.middle_name} {emp.last_name}
                   </div>
                   <div>{emp.dept_name}</div>
                   <div>{emp.job}</div>
-                  <div>${emp.salary}</div>
+                  <div className="font-bold text-black">${emp.salary}</div>
                 </div>
               </div>
             ))}
