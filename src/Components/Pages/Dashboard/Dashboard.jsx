1e2eb1fccf0feb1e9816b7782a02dce2d67ffc9a
jivan aryal <jeevanaryal7947@gmail.com>
2023-07-04 19:24:33 +0545

    hello

--- src/Components/Pages/Dashboard/Dashboard.jsx
+++ src/Components/Pages/Dashboard/Dashboard.jsx
@@ -47,13 +47,13 @@ const Dashboard = () => {
 
   return (
     <div className="">
-      <div className="flex justify-between">
-        <div className="grid grid-cols-1 gap-3 h-96  md:grid-cols-2 w-6/12">
+      <div className="flex gap-10">
+        <div className="grid grid-cols-1 gap-5   md:grid-cols-2 w-10/12">
           {data.map((val, i) => (
             <Link to={val.path} key={i}>
               <div
                 style={{ backgroundColor: val.colors }}
-                className="rounded-lg flex flex-col gap-2 text-sm shadow-md shadow-blue-400 py-10 mx-2 sm:mx-4 md:mx-6 text-center hover:transition-all hover:scale-105 hover:delay-200 hover:duration-1000"
+                className="rounded-lg flex flex-col gap-2 items-center text-sm shadow-md shadow-blue-400 py-10 mx-2 sm:mx-4 md:mx-6 text-center hover:transition-all hover:scale-105 hover:delay-200 hover:duration-1000"
               >
                 <div className="text-lg">{val.title}</div>
                 <div>{val.num}</div>
@@ -62,25 +62,23 @@ const Dashboard = () => {
             </Link>
           ))}
         </div>
-        <div className="employee_data px-6 ">
+        <div className="employee_data px-6 shadow-xl bg-white rounded-lg w-full mx-4">
           <h1 className="text-2xl font-bold mb-4">Top Employees</h1>
-          <div className="shadow-xl bg-white rounded-lg p-4">
-            {employee.slice(0, 4).map((emp, index) => (
+          <div>
+            {employee.slice(0, 6).map((emp, index) => (
               <div key={index} className="flex items-center mb-4">
                 <img
                   src={`http://192.168.18.7:5000/${emp.image}`}
                   alt="Employee"
-                  className="w-16 h-16 rounded-full mr-5"
+                  className="w-12 h-12 rounded-full mr-5"
                 />
-                <div className="flex gap-4 font-bold items-center justify-start">
-                  <div className="text-gray-600">
+                <div className="grid grid-cols-5 gap-4 w-full  text-[#5E5E5E] font-bold items-center  ">
+                  <div className="text-gray-600  col-span-2">
                     {emp.first_name} {emp.middle_name} {emp.last_name}
                   </div>
-                  <div className="text-gray-600 mb-2">{emp.job}</div>
-                  <div className="text-gray-600 mb-2">{emp.dept_name}</div>
-                  <div className="text-gray-600 mb-2 text-center">
-                    ${emp.salary}
-                  </div>
+                  <div className="  ">{emp.job}</div>
+                  <div className="  ">{emp.dept_name}</div>
+                  <div className="  ">${emp.salary}</div>
                 </div>
               </div>
             ))}
